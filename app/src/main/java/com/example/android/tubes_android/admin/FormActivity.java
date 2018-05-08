781e888b90bfc0e6732ef6f859817338b6a7678a
bahrilanwar <36129132+bahrilanwar@users.noreply.github.com>
2018-05-09 02:23:37 +0700

    Spinner rName Clear

--- app/src/main/java/com/example/android/tubes_android/admin/FormActivity.java
+++ app/src/main/java/com/example/android/tubes_android/admin/FormActivity.java
@@ -6,10 +6,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.android.tubes_android.ListKelasModel;
@@ -20,15 +23,19 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class FormActivity extends AppCompatActivity {
 
-    private EditText RC, RN, time, day;
-    private String status;
+    private EditText RC, time, day;
+    private String status, rName;
     private Button btnBook;
-    private RadioGroup rg;
+    private Spinner RN;
     private RadioButton rbB, rbO;
 
     private FirebaseAuth mAuth;
 
     private DatabaseReference ref;
+
+    private String[] room = {
+            "KU2.01.01", "KU2.01.02", "KU2.01.03", "KU2.01.04", "KU2.01.05", "KU2.01.06", "KU2.01.07", "KU2.01.08", "KU2.01.09"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,10 +50,14 @@ public class FormActivity extends AppCompatActivity {
         RN = findViewById(R.id.lblRoomName);
         time = findViewById(R.id.startTime);
         day = findViewById(R.id.lblDay);
-        rg = findViewById(R.id.rbg);
         rbB = findViewById(R.id.rbBooked);
         rbO = findViewById(R.id.rbOpened);
+//        room = findViewById(R.id.rbg);
         btnBook = findViewById(R.id.book);
+
+
+
+        spinnerListener();
 
         ref = FirebaseDatabase.getInstance().getReference("classlist");
 
@@ -54,6 +65,27 @@ public class FormActivity extends AppCompatActivity {
 
     }
 
+    public void spinnerListener() {
+        // inisialiasi Array Adapter dengan memasukkan string array di atas
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, room);
+
+        // mengeset Array Adapter tersebut ke Spinner
+        RN.setAdapter(adapter);
+
+        // mengeset listener untuk mengetahui saat item dipilih
+        RN.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                // mengambil value Spinner yang dipilih (diambil dari adapter)
+                rName = adapter.getItem(i);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
 
     public void input(View view) {
         // Check which radio button was clicked
@@ -70,7 +102,7 @@ public class FormActivity extends AppCompatActivity {
         ref.child(key).setValue(new ListKelasModel(
                 key,
                 RC.getText().toString(),
-                RN.getText().toString(),
+                rName,
                 time.getText().toString(),
                 day.getText().toString(),
                 status
